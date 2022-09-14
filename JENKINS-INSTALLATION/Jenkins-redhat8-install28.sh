5f014293a899467b5f6a72acaae600f3b7505101
pasima-m <111281963+pasima-m@users.noreply.github.com>
2022-09-14 13:21:34 +0100

    Update Jenkins-redhat8-install28.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
@@ -1,5 +1,5 @@
 # CREATE HOSTNAME
-sudo hostname auto
+sudo hostnamectl set-hostname -y
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
